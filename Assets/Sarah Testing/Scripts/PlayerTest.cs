1e46c8637d3df8cc9772e1a411b7deb4c15f0a4e
sarahPC <sarahapplebaum.sia@gmail.com>
2022-07-26 21:59:27 -0400

    worked on attack player enemy affector and agnt script

---
+++ Assets/Sarah Testing/Scripts/PlayerTest.cs
@@ -0,0 +1,22 @@
+using System;
+using ActionGameFramework.Health;
+using Core.Utilities;
+using TowerDefense.Level;
+using TowerDefense.Towers.Placement;
+using TowerDefense.UI.HUD;
+using UnityEngine;
+
+public class PlayerTest : Targetable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/Agents/PlayerAttackAgent.cs
+++ Assets/UnityTechnologies/TowerDefenseTemplate/Scripts/TowerDefense/Agents/PlayerAttackAgent.cs
@@ -6,7 +6,8 @@ using TowerDefense.Affectors;
 using TowerDefense.Towers;
 using Core.Health;
 using TowerDefense.Nodes;
-
+using System;
+using TowerDefense.Targetting;
 namespace TowerDefense.Agents{
 public class PlayerAttackAgent : Agent
 {
@@ -18,104 +19,147 @@ public class PlayerAttackAgent : Agent
 
         protected override bool isAtDestination => base.isAtDestination;
 
-        //this enemy strictly attacks the player, attempting to stop them from killing other enemies
-        // Start is called before the first frame update
+        protected Targetter targettingScr;
 
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
+        List<GameObject> listOfTargets;
 
-        public override bool Equals(object other)
-        {
-            return base.Equals(other);
-        }
+        public AttackAffectorPlayer attacking;
 
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+        public PlayerTest player;
+        protected bool m_IsAttacking;
 
-        public override void TakeDamage(float damageValue, Vector3 damagePoint, IAlignmentProvider alignment)
-        {
-            base.TakeDamage(damageValue, damagePoint, alignment);
-        }
-
-        protected override void Kill()
-        {
-            base.Kill();
-        }
-
-        public override void SetNode(Node node)
-        {
-            base.SetNode(node);
-        }
-
-        public override void Remove()
-        {
-            base.Remove();
-        }
-
-        public override void Initialize()
-        {
-            base.Initialize();
-        }
-
-        public override void GetNextNode(Node currentlyEnteredNode)
-        {
-            base.GetNextNode(currentlyEnteredNode);
-        }
-
-        public override void MoveToNode()
-        {
-            base.MoveToNode();
-        }
-
-        public override void HandleDestinationReached()
-        {
-            base.HandleDestinationReached();
-        }
-
-        protected override void Awake()
-        {
-            base.Awake();
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-        }
-
-        protected override void NavigateTo(Vector3 nextPoint)
-        {
-            base.NavigateTo(nextPoint);
-        }
-
-        protected override void LazyLoad()
-        {
-            base.LazyLoad();
-        }
-
-        protected override void OnCompletePathUpdate()
-        {
-            base.OnCompletePathUpdate();
-        }
-
-        protected override void PathUpdate()
-        {
-            throw new System.NotImplementedException();
-        }
+        protected PlayerTest m_TargetTower;
+        
+        
 
         protected override void OnPartialPathUpdate()
         {
-            throw new System.NotImplementedException();
+            if (!isPathBlocked)
+			{
+				state = State.OnCompletePath;
+				return;
+			}
+            attacking.towerTargetter.transform.position = m_NavMeshAgent.pathEndPosition;
+            player = GetClosestTower();
+            
+            attacking.towerTargetter.transform.position = m_NavMeshAgent.pathEndPosition;
+			PlayerTest tower = GetClosestTower();
+			if (tower != m_TargetTower)
+			{
+				// if the current target is to be replaced, unsubscribe from removed event
+				if (m_TargetTower != null)
+				{
+					m_TargetTower.removed -= OnTargetTowerDestroyed;
+				}
+				
+				// assign target, can be null
+				m_TargetTower = tower;
+				
+				// if new target found subscribe to removed event
+				if (m_TargetTower != null)
+				{
+					m_TargetTower.removed += OnTargetTowerDestroyed;
+				}
+			}
+			if (m_TargetTower == null)
+			{
+				return;
+			}
+			float distanceToTower = Vector3.Distance(transform.position, m_TargetTower.transform.position);
+			if (!(distanceToTower < attacking.towerTargetter.effectRadius))
+			{
+				return;
+			}
+			if (!attacking.enabled)
+			{
+				attacking.towerTargetter.transform.position = transform.position;
+				attacking.enabled = true;
+			}
+			state = State.Attacking;
+			m_NavMeshAgent.isStopped = true;
+            
+            
         }
 
-        protected override void OnDrawGizmosSelected()
+        protected override void LazyLoad()
+		{
+			base.LazyLoad();
+			if (attacking == null)
+			{
+				attacking = GetComponent<AttackAffectorPlayer>();
+			}
+		}
+
+        private PlayerTest GetClosestTower()
         {
-            base.OnDrawGizmosSelected();
+            if (attacking.towerTargetter.GetTarget().gameObject.tag == "Player")
+            {
+                Debug.Log("player found");
+            }
+            var towerController = attacking.towerTargetter.GetTarget() as PlayerTest;
+			return towerController;
         }
 
-}
+        public override void Remove()
+		{
+			base.Remove();
+			if (m_TargetTower != null)
+			{
+				m_TargetTower.removed -= OnTargetTowerDestroyed;
+			}
+			attacking.enabled = false;
+			m_TargetTower = null;
+		}
+
+        protected override void PathUpdate()
+        {
+            switch (state)
+			{
+				case State.OnCompletePath:
+					OnCompletePathUpdate();
+					break;
+				case State.OnPartialPath:
+					OnPartialPathUpdate();
+					break;
+				case State.Attacking:
+					AttackingUpdate();
+					break;
+			}
+
+            
+
+        }
+
+        protected virtual void OnTargetTowerDestroyed(DamageableBehaviour tower)
+		{
+			if (m_TargetTower == tower)
+			{
+				m_TargetTower.removed -= OnTargetTowerDestroyed;
+				m_TargetTower = null;
+			}
+		}
+
+        protected void AttackingUpdate()
+		{
+			if (player != null)
+			{
+				return;
+			}
+			MoveToNode();
+
+			// Resume path once blocking has been cleared
+			m_IsAttacking = false;
+			m_NavMeshAgent.isStopped = false;
+			attacking.enabled = false;
+			state = isPathBlocked ? State.OnPartialPath : State.OnCompletePath;
+			// Move the Targetter back to the agent's position
+			attacking.towerTargetter.transform.position = transform.position;
+		}
+
+        //this enemy strictly attacks the player, attempting to stop them from killing other enemies
+        // Start is called before the first frame update
+
+
+    }
 
 }
